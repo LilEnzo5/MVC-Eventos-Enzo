48e403903fcd07f4df933e62ecb98c28405c24c9
USER5 <enzo_wt@hotmail.com>
2026-04-18 19:44:36 -0500

    Commit Code evento

--- example/models/Evento.java
+++ example/models/Evento.java
@@ -1,4 +1,33 @@
 package models;
 
+import java.util.ArrayList;
+
+
 public class Evento {
+    private String Evento;
+    private String Fecha;
+    private ArrayList<String> invitados;
+
+    public Evento(String nombre, String fecha) {
+        this.nombre = nombre;
+        this.fecha = fecha;
+        this.invitados = new ArrayList<>();
+
+    }
+
+    public void agregarInvitado(String invitado) {
+        invitados.add(invitado);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public ArrayList<String> getInvitados() {
+        return invitados;
+    }
 }
